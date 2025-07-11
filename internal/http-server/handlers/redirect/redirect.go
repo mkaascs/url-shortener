b8c474527b3f6f2e5702cc6ef03d6aec388393e2
Maxim <teakeyx@gmail.com>
2025-07-11 19:35:46 +0300

    add not found status error

--- internal/http-server/handlers/redirect/redirect.go
+++ internal/http-server/handlers/redirect/redirect.go
@@ -1,12 +1,14 @@
 package redirect
 
 import (
+	"errors"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
 	"net/http"
 	"url-shortener/internal/lib/api/response"
 	"url-shortener/internal/logging/sl"
+	"url-shortener/internal/storage"
 )
 
 type URLGetter interface {
@@ -30,6 +32,14 @@ func New(log *slog.Logger, getter URLGetter) http.HandlerFunc {
 		}
 
 		url, err := getter.GetURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url with alias not found")
+			response.RenderError(w, r,
+				http.StatusNotFound,
+				"url with alias not found")
+			return
+		}
+
 		if err != nil {
 			log.Error("failed to get url", sl.Error(err))
 			response.RenderError(w, r,
